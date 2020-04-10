eb286679c5a6b3d8b637bad8c72eaa893fa925c3
peria1 <37117878+peria1@users.noreply.github.com>
2020-04-10 11:15:33 -0700

    Added everything necessary to return predictions using the network in eval mode. Eval mode and train mode actually return different things! I needed to move prep_display from eval.py to here (as local_prep_display) because I don't have args/parser/config available the way Bolya uses them, here in extract4trainer. I am not 100% sure these predictions are being executed correctly, because they are very poor and I had thought the network was already trained on COCO. So I am confused. But, everything ran!

--- extract4trainer.py
+++ extract4trainer.py
@@ -5,6 +5,7 @@ Created on Wed Mar 11 15:07:37 2020
 @author: Bill
 """
 from train import  NetWithLoss, CustomDataParallel, MultiBoxLoss, prepare_data
+from collections import defaultdict
 
 #
 # I import Bolya's yolact/data subdirectory as D. This will pick
@@ -15,10 +16,169 @@ import data as D
 from utils.augmentations import SSDAugmentation #, FastBaseTransform, BaseTransform
 import torch
 from yolact import Yolact
+#from eval import prep_display # oops no, clone and modify here as local_prep_display
+from layers.output_utils import postprocess, undo_image_transformation
+
+
 import matplotlib.pyplot as plt
 import matplotlib
 import numpy as np
 import copy
+from utils import timer
+import cv2
+from torch.autograd import Variable
+
+color_cache = defaultdict(lambda: {})
+
+
+def local_prep_display(dets_out, img, h, w, undo_transform=True, class_color=False, mask_alpha=0.45, fps_str=''):
+    """
+    Note: If undo_transform=False then im_h and im_w are allowed to be None.
+    
+    I don't have args available, so I need to create it and fill it with the defaults. 
+    
+    
+    """
+    print('local_prep_display, type(dets_out) is',type(dets_out))
+    
+    top_k = 5
+    score_threshold = 0.0
+    display_masks = True
+    display_text = True
+    display_bboxes = True
+    display_scores = True
+    display_fps = False
+
+    if undo_transform:
+        img_numpy = undo_image_transformation(img, w, h)
+        img_gpu = torch.Tensor(img_numpy).cuda()
+    else:
+        img_gpu = img / 255.0
+        h, w, _ = img.shape
+    
+    with timer.env('Postprocess'):
+        save =D.cfg.rescore_bbox
+        D.cfg.rescore_bbox = True
+#        t = postprocess(dets_out, w, h, visualize_lincomb = args.display_lincomb,
+#                                        crop_masks        = args.crop,
+#                                        score_threshold   = args.score_threshold)
+        t = postprocess(dets_out, w, h, visualize_lincomb = False,
+                                        crop_masks        = True,
+                                        score_threshold   = score_threshold)
+
+        D.cfg.rescore_bbox = save
+
+    with timer.env('Copy'):
+#        idx = t[1].argsort(0, descending=True)[:args.top_k]
+        idx = t[1].argsort(0, descending=True)[:top_k]
+        
+        if D.cfg.eval_mask_branch:
+            # Masks are drawn on the GPU, so don't copy
+            masks = t[3][idx]
+        classes, scores, boxes = [x[idx].cpu().detach().numpy() for x in t[:3]]
+
+    num_dets_to_consider = min(top_k, classes.shape[0])
+    for j in range(num_dets_to_consider):
+        if scores[j] < score_threshold:
+            num_dets_to_consider = j
+            break
+
+    # Quick and dirty lambda for selecting the color for a particular index
+    # Also keeps track of a per-gpu color cache for maximum speed
+    def get_color(j, on_gpu=None):
+        global color_cache
+        color_idx = (classes[j] * 5 if class_color else j * 5) % len(D.COLORS)
+        
+        if on_gpu is not None and color_idx in color_cache[on_gpu]:
+            return color_cache[on_gpu][color_idx]
+        else:
+            color = D.COLORS[color_idx]
+            if not undo_transform:
+                # The image might come in as RGB or BRG, depending
+                color = (color[2], color[1], color[0])
+            if on_gpu is not None:
+                color = torch.Tensor(color).to(on_gpu).float() / 255.
+                color_cache[on_gpu][color_idx] = color
+            return color
+
+    # First, draw the masks on the GPU where we can do it really fast
+    # Beware: very fast but possibly unintelligible mask-drawing code ahead
+    # I wish I had access to OpenGL or Vulkan but alas, I guess Pytorch tensor operations will have to suffice
+    if display_masks and D.cfg.eval_mask_branch and num_dets_to_consider > 0:
+        # After this, mask is of size [num_dets, h, w, 1]
+        masks = masks[:num_dets_to_consider, :, :, None]
+        
+        # Prepare the RGB images for each mask given their color (size [num_dets, h, w, 1])
+        colors = torch.cat([get_color(j, on_gpu=img_gpu.device.index).view(1, 1, 1, 3) for j in range(num_dets_to_consider)], dim=0)
+        masks_color = masks.repeat(1, 1, 1, 3) * colors * mask_alpha
+
+        # This is 1 everywhere except for 1-mask_alpha where the mask is
+        inv_alph_masks = masks * (-mask_alpha) + 1
+        
+        # I did the math for this on pen and paper. This whole block should be equivalent to:
+        #    for j in range(num_dets_to_consider):
+        #        img_gpu = img_gpu * inv_alph_masks[j] + masks_color[j]
+        masks_color_summand = masks_color[0]
+        if num_dets_to_consider > 1:
+            inv_alph_cumul = inv_alph_masks[:(num_dets_to_consider-1)].cumprod(dim=0)
+            masks_color_cumul = masks_color[1:] * inv_alph_cumul
+            masks_color_summand += masks_color_cumul.sum(dim=0)
+
+        img_gpu = img_gpu * inv_alph_masks.prod(dim=0) + masks_color_summand
+    
+    if display_fps:
+            # Draw the box for the fps on the GPU
+        font_face = cv2.FONT_HERSHEY_DUPLEX
+        font_scale = 0.6
+        font_thickness = 1
+
+        text_w, text_h = cv2.getTextSize(fps_str, font_face, font_scale, font_thickness)[0]
+
+        img_gpu[0:text_h+8, 0:text_w+8] *= 0.6 # 1 - Box alpha
+
+
+    # Then draw the stuff that needs to be done on the cpu
+    # Note, make sure this is a uint8 tensor or opencv will not anti alias text for whatever reason
+    img_numpy = (img_gpu * 255).byte().cpu().numpy()
+
+    if display_fps:
+        # Draw the text on the CPU
+        text_pt = (4, text_h + 2)
+        text_color = [255, 255, 255]
+
+        cv2.putText(img_numpy, fps_str, text_pt, font_face, font_scale, text_color, font_thickness, cv2.LINE_AA)
+    
+    if num_dets_to_consider == 0:
+        return img_numpy
+
+    if display_text or display_bboxes:
+        for j in reversed(range(num_dets_to_consider)):
+            x1, y1, x2, y2 = boxes[j, :]
+            color = get_color(j)
+            score = scores[j]
+
+            if display_bboxes:
+                cv2.rectangle(img_numpy, (x1, y1), (x2, y2), color, 1)
+
+            if display_text:
+                _class = D.cfg.dataset.class_names[classes[j]]
+                text_str = '%s: %.2f' % (_class, score) if display_scores else _class
+
+                font_face = cv2.FONT_HERSHEY_DUPLEX
+                font_scale = 0.6
+                font_thickness = 1
+
+                text_w, text_h = cv2.getTextSize(text_str, font_face, font_scale, font_thickness)[0]
+
+                text_pt = (x1, y1 - 3)
+                text_color = [255, 255, 255]
+
+                cv2.rectangle(img_numpy, (x1, y1), (x1 + text_w, y1 - text_h - 4), color, -1)
+                cv2.putText(img_numpy, text_str, text_pt, font_face, font_scale, text_color, font_thickness, cv2.LINE_AA)
+            
+    
+    return img_numpy
+
 
 def npscl(xin):
     x = copy.copy(xin)
@@ -126,23 +286,42 @@ if __name__ == '__main__':
     #   function, and use it to process datum. 
     #
     net = Yolact()
-    net.train()
     net.init_weights(backbone_path='weights/' + D.cfg.backbone.path)
-
-    criterion = MultiBoxLoss(num_classes=D.cfg.num_classes,
-                             pos_threshold=D.cfg.positive_iou_threshold,
-                             neg_threshold=D.cfg.negative_iou_threshold,
-                             negpos_ratio=D.cfg.ohem_negpos_ratio)
-
+    net.eval()
     
-    net_cdp = CustomDataParallel(NetWithLoss(net, criterion))
-    net_cdp.cuda()
+    net.detect.use_fast_nms = True
+    net.detect.use_cross_class_nms = False
 
-    losses = net_cdp(datum)
-    losses = { k: (v).mean() for k,v in losses.items() } # Mean here because Dataparallel
-    loss = sum([losses[k] for k in losses])
+#    just_one = net(images[0].unsqueeze(0).cuda())
     
-    print('Whoa! We made it! Loss is',loss)
+    img, gt, gt_masks, h, w, num_crowd = dataset.pull_item(0)
+                
+    batch = img.unsqueeze(0).cuda()
+    preds = net(batch)
+
+#    hackpreds = []
+#    hackpreds.append(preds)
+
+    img_numpy = local_prep_display(preds, img, h, w)
+
+#    preds = []
+#    for im in images:
+#        preds.append(net(im.unsqueeze(0).cuda()))
+
+#    criterion = MultiBoxLoss(num_classes=D.cfg.num_classes,
+#                             pos_threshold=D.cfg.positive_iou_threshold,
+#                             neg_threshold=D.cfg.negative_iou_threshold,
+#                             negpos_ratio=D.cfg.ohem_negpos_ratio)
+#
+#    
+#    net_cdp = CustomDataParallel(NetWithLoss(net, criterion))
+#    net_cdp.cuda()
+#
+#    losses = net_cdp(datum)
+#    losses = { k: (v).mean() for k,v in losses.items() } # Mean here because Dataparallel
+#    loss = sum([losses[k] for k in losses])
+#    
+#    print('Whoa! We made it! Loss is',loss)
 
 #
 #  Below here I am trying to call the augmentation by hand, to quickly test
